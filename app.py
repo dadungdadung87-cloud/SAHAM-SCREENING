6625bef067a78a45bea0daaa110576f2a783abaa
dadungdadung87-cloud <dadungdadung87@gmail.com>
2026-07-04 15:11:39 +0000

    menghapus tombool refresh

--- app.py
+++ app.py
@@ -157,11 +157,6 @@ if not df_hasil.empty:
     met3.markdown(f"<div class='metric-container'><h3>📈 Fase Uptrend</h3><h2 style='color: #60a5fa;'>{total_uptrend} Saham</h2></div>", unsafe_allow_html=True)
     
     st.markdown("### 🎛️ Panel Filter Lengkap")
-    
-    # Tombol untuk menarik data paling baru dari market
-if st.button("🔄 Perbarui Data Market Sekarang"):
-    st.cache_data.clear() # Menghapus ingatan lama
-    st.rerun() # Memuat ulang web secara instan
 
     # FITUR BARU: Kolom Pencarian Saham
     search_ticker = st.text_input("🔍 Cari Kode Saham Spesifik (Contoh: BBCA, BMRI)", "")
